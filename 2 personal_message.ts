569200288ee03981db1cfc20dd99080001725d14
sana575 <148882072+sana575@users.noreply.github.com>
2024-03-10 01:13:47 +0500

    Add files via upload

---
+++ 2 personal_message.ts
@@ -0,0 +1,17 @@
+// let personName:string ="sana";
+// console.log(`hello ${personName},would like to learn some python today?`);
+
+
+// // Q4,famous_Quot.
+
+// console.log('Albert Einstein once said,"A person who never made a mistake never tried anything new"');
+
+//Q5,famous_quot 2.
+
+let famous_person:string = "albert Einstein";
+
+// let message:string = "A person who never made a mistake never tried anything new .";
+// console.log(`${famous_person} once said,${message}`);
+
+let message:string = `${famous_person} once said,"A person who never made a mistake never tried anything new. "`;
+console.log(message);
